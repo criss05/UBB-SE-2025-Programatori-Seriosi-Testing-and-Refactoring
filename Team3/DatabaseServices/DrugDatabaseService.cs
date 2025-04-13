7ea693afaeb726be0a5b752f964a562b28964ac0
Barnut Cristiana <119601150+criss05@users.noreply.github.com>
2025-04-13 04:17:35 +0300

    some refactoring for services.

--- Team3/DatabaseServices/DrugDatabaseService.cs
+++ Team3/DatabaseServices/DrugDatabaseService.cs
@@ -19,15 +19,18 @@ namespace Team3.DatabaseServices
     /// </summary>
     public class DrugDatabaseService :IDrugDatabaseService
     {
-        private static DrugDatabaseService? instance;
         private static readonly object LockObject = new object();
+        private static DrugDatabaseService? instance;
         private readonly Config config;
 
         private DrugDatabaseService()
         {
-            config = Config.Instance;
+            this.config = Config.Instance;
         }
 
+        /// <summary>
+        /// Gets dingleton instance of the DrugDatabaseService.
+        /// </summary>
         public static DrugDatabaseService Instance
         {
             get
@@ -39,10 +42,17 @@ namespace Team3.DatabaseServices
                         instance = new DrugDatabaseService();
                     }
                 }
+
                 return instance;
-            }   
+            }
         }
 
+        /// <summary>
+        /// Gets a drug by its ID from the database.
+        /// </summary>
+        /// <param name="Id">drug id.</param>
+        /// <returns>drug.</returns>
+        /// <exception cref="Exception">throw error.</exception>
         public Drug getDrugById(int Id)
         {
             const string query = "SELECT * FROM drugs WHERE id = @id;";
@@ -51,24 +61,20 @@ namespace Team3.DatabaseServices
             {
                 SqlConnection connection = new SqlConnection(Config.DATABASE_CONNECTION_STRING);
 
-
                 connection.Open();
 
                 SqlCommand command = new SqlCommand(query, connection);
-
 
                 command.Parameters.AddWithValue("@id", Id);
 
                 SqlDataReader reader = command.ExecuteReader();
                 reader.Read();
                 return new Drug(reader.GetInt32(0), reader.GetString(1), reader.GetString(2));
-       
             }
             catch (Exception e)
             {
                 throw new Exception("Error getting drug", e);
             }
-
         }
     }
 }
---
+++ Team3/DatabaseServices/EquipmentDatabaseService.cs
@@ -0,0 +1,20 @@
+﻿// <copyright file="EquipmentDatabaseService.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace Team3.DatabaseServices
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Team3.DatabaseServices;
+
+    /// <summary>
+    /// This class implements the IEquipmentDatabaseService interface.
+    /// </summary>
+    public class EquipmentDatabaseService : IEquipmentDatabaseService
+    {
+    }
+}
